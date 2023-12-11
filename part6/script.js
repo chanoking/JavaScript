8c8142e0da7c3e73d7c260a33c14f0f8ae612ebf
진찬호 <chanhojin94@kakao.com>
2023-12-11 17:00:28 +0900

    behind scences

---
+++ part6/script.js
@@ -0,0 +1,64 @@
+// function calcAge(birthYear) {
+//   const age = 2037 - birthYear;
+
+//   function printAge() {
+//     const output = `${firstName} are ${age}, born in ${birthYear}`;
+//     console.log(output);
+//     if (birthYear >= 1981 && birthYear <= 1996) {
+//       var millenial = true;
+//       const str = `Oh, and you're a millenial, ${firstName}`;
+//       console.log(str);
+//       function add(a, b){
+//         return a + b;
+//       }
+//     }
+//   }
+//   printAge();
+//   // console.log(millenial);
+//   return age;
+// }
+
+// const firstName = "Jonas";
+// calcAge(1991);
+// console.log(me);
+// console.log(job);
+// console.log(year);
+
+// var me = "chano";
+// let job = "software developer";
+// const year = 1994;
+"use strict";
+// console.log(this);
+
+// const calcAge = function (birthYear) {
+//   console.log(2037 - birthYear);
+//   console.log(this);
+// };
+
+// calcAge(1991);
+
+// function baseball(a, b) {
+//   console.log(this);
+//   return a + b;
+// }
+// baseball(1, 2);
+
+// const me = (a, b) => {
+//   console.log(this);
+//   return a + b;
+// };
+// me(1, 2);
+
+// const jonas = {
+//   year: 1994,
+//   calcAge: function () {
+//     console.log(this);
+//   },
+// };
+// jonas.calcAge();
+
+// const addExpr = function (a, b) {
+//   console.log(arguments);
+//   return a + b;
+// };
+// addExpr(2, 3);
