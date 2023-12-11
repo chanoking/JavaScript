78cebc1d90abdfef3b922ed64e64debfd9cc5268
진찬호 <chanhojin94@kakao.com>
2023-12-11 22:01:23 +0900

    Data Structures and Modern Operators

--- part6/script.js
+++ part6/script.js
@@ -62,3 +62,65 @@
 //   return a + b;
 // };
 // addExpr(2, 3);
+
+// let lastName = "Williams";
+// let oldLastName = lastName;
+// lastName = "Davis";
+// console.log(lastName, oldLastName);
+// console.log(lastName, oldLastName);
+
+// const jessica = {
+//   firstName: "Jessica",
+//   lastName: "Williams",
+//   age: 27,
+// };
+// const marriedJessica = jessica;
+// marriedJessica.lastName = "Davis";
+// console.log("Before marriage:", jessica);
+// console.log("After marriage:", marriedJessica);
+
+"use strict";
+
+// Data needed for a later exercise
+const flights =
+  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
+
+// Data needed for first part of the section
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+};
+
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+// console.log(menu);
+
+const add = function (...numbers) {
+  console.log(numbers);
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  console.log(sum);
+  return sum;
+};
+add(2, 3);
+add(1, 2, 3, 4, 5);
+add(1, 2, 3, 4, 5, 6, 7, 8, 9);
