a979c84f4617a1bb39a8e8e8edc57ac65dcdf863
진찬호 <chanhojin94@kakao.com>
2023-12-17 12:58:11 +0900

    다양한 메소드 활용

--- part6/script.js
+++ part6/script.js
@@ -81,46 +81,108 @@
 
 "use strict";
 
-// Data needed for a later exercise
-const flights =
-  "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
+// // Data needed for a later exercise
+// const flights =
+//   "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
 
-// Data needed for first part of the section
-const restaurant = {
-  name: "Classico Italiano",
-  location: "Via Angelo Tavanti 23, Firenze, Italy",
-  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
-  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
-  mainMenu: ["Pizza", "Pasta", "Risotto"],
+// // Data needed for first part of the section
+// const restaurant = {
+//   name: "Classico Italiano",
+//   location: "Via Angelo Tavanti 23, Firenze, Italy",
+//   categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+//   starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+//   mainMenu: ["Pizza", "Pasta", "Risotto"],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
+//   openingHours: {
+//     thu: {
+//       open: 12,
+//       close: 22,
+//     },
+//     fri: {
+//       open: 11,
+//       close: 23,
+//     },
+//     sat: {
+//       open: 0, // Open 24 hours
+//       close: 24,
+//     },
+//   },
+// };
+
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+// // console.log(menu);
+
+// const add = function (...numbers) {
+//   console.log(numbers);
+//   let sum = 0;
+//   for (let i = 0; i < numbers.length; i++) {
+//     sum += numbers[i];
+//   }
+//   console.log(sum);
+//   return sum;
+// };
+// add(2, 3);
+// add(1, 2, 3, 4, 5);
+// add(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+// const announcement =
+//   "Hello, there! Please going to the 59 exit! Again 59 exit!";
+
+// const newAnnouncement = announcement.replaceAll("exit", "entrance");
+// console.log(newAnnouncement);
+
+// const againAnnouncement = announcement.replace(/exit/g, "entrance");
+// console.log(againAnnouncement);
+
+// let f;
+
+// const g = function () {
+//   const a = 23;
+//   f = function () {
+//     console.log(a * 2);
+//   };
+// };
+
+// g();
+// f();
+
+// const arr = [44, -10, 4, 9, 20];
+// const arrNew = arr.reduce((acc, cur) => acc + cur, 0);
+// console.log(arrNew);
+
+// const convertString = function (title) {
+//   const capitalize = (str) => str[0].toUpperCase() + str.slice(1);
+//   const exceptions = ["an", "and", "on", "a", "of", "the", "with", "for"];
+//   const convertCaptializae = title
+//     .toLowerCase()
+//     .split(" ")
+//     .map((word) => (exceptions.includes(word) ? word : capitalize(word)))
+//     .join(" ");
+//   return capitalize(convertCaptializae);
+// };
+
+const convertString = (title) => {
+  const exceptions = [
+    "a",
+    "an",
+    "with",
+    "with",
+    "for",
+    "the",
+    "on",
+    "to",
+    "of",
+  ];
+  const capitalize = (str) => str[0].toUpperCase() + str.slice(1);
+  const convertTitle = title
+    .toLowerCase()
+    .split(" ")
+    .map((w) => (exceptions.includes(w) ? w : capitalize(w)))
+    .join(" ");
+
+  return capitalize(convertTitle);
 };
 
-const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
-// console.log(menu);
-
-const add = function (...numbers) {
-  console.log(numbers);
-  let sum = 0;
-  for (let i = 0; i < numbers.length; i++) {
-    sum += numbers[i];
-  }
-  console.log(sum);
-  return sum;
-};
-add(2, 3);
-add(1, 2, 3, 4, 5);
-add(1, 2, 3, 4, 5, 6, 7, 8, 9);
+console.log(convertString("This is an application for you"));
+console.log(convertString("No, it isnt mine of me"));
+console.log(convertString("Thank you, very much on the whole"));
