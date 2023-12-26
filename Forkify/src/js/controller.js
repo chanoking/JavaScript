d3407a4d14841dcc7b57a1b6b11fd522c729b7da
진찬호 <chanhojin94@kakao.com>
2023-12-26 10:58:21 +0900

    query search

--- Forkify/src/js/controller.js
+++ Forkify/src/js/controller.js
@@ -2,12 +2,7 @@ import "core-js/stable";
 import "regenerator-runtime/runtime";
 import * as model from "./model.js";
 import recipeView from "./views/recipeView.js";
-
-const recipeContainer = document.querySelector(".recipe");
-
-// https://forkify-api.herokuapp.com/v2
-
-///////////////////////////////////////
+import searchView from "./views/searchView.js";
 
 const controlRecipes = async function () {
   try {
@@ -27,15 +22,22 @@ const controlRecipes = async function () {
 
 const controlSearchResults = async function () {
   try {
-    await model.loadSearchResults("pizza");
-    console.log(state.search.results);
+    // 1) Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // 2) Load search results
+    await model.loadSearchResults(query);
+
+    // 3) Render results
+    console.log(model.state.search.results);
   } catch (err) {
     console.log(err);
   }
 };
-controlSearchResults();
 
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
 };
 init();
